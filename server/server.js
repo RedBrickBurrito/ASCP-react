05e2965956c5dbed42767ee39724da3aaced14ce
cemarv25 <cesar.martinez0010@outlook.com>
2021-11-19 13:06:49 -0700

    fixed socket messages

--- server/server.js
+++ server/server.js
@@ -25,43 +25,50 @@ const io = require('socket.io')(http, {
   },
 });
 
-io.on("connection", (socket) => {
-  console.log("client connected");
-  socket.on("disconnect", () => {
-    console.log("cliente disconnected");
+io.on('connection', (socket) => {
+  console.log('client connected');
+  socket.on('disconnect', () => {
+    console.log('cliente disconnected');
   });
 
-  socket.on("enviar-mensaje", (msg) => {
-    socket.emit("Mensaje ASCP", msg);
+  socket.on('enviar-mensaje', (msg) => {
+    socket.emit('Mensaje ASCP', msg);
   });
 
-  socket.on("Mensaje ASCP", (msg) => {
-    const message = decodeDesECB(msg.data, sharedKey);
-    console.log("mensaje recibido ", msg.data);
-    console.log("mensaje desencriptado ", message);
-    socket.broadcast.emit("recibir-mensaje", { function: 1, data: message });
-  });
-
-  socket.on("SIMP_INIT_COMM", (publicKey) => {
-    isAlice = false;
-    othersKey = publicKey;
-    console.log(
-      "se inicio una comunicacion por parte de alice con llave publica ",
-      othersKey
-    );
-    socket.broadcast.emit("set-bob", sharedKey);
-  });
-
-  socket.on("SIMP_KEY_COMPUTED", (publicKey) => {
-    othersKey = publicKey;
-    console.log("se recibio la llave publica de bob ", othersKey);
-    computeSharedKey();
-    socket.broadcast.emit("shared-key", sharedKey);
+  socket.on('Mensaje ASCP', (msg) => {
+    switch (msg.function) {
+      case 1:
+        const message = decodeDesECB(msg.data, sharedKey);
+        console.log('mensaje recibido ', msg.data);
+        console.log('mensaje desencriptado ', message);
+        socket.broadcast.emit('recibir-mensaje', {
+          function: 1,
+          data: message,
+        });
+        break;
+      case 2:
+        isAlice = false;
+        othersKey = msg.data;
+        console.log(
+          'se inicio una comunicacion por parte de alice con llave publica ',
+          othersKey
+        );
+        socket.broadcast.emit('set-bob', msg.data);
+        break;
+      case 3:
+        othersKey = msg.data;
+        console.log('se recibio la llave publica de bob ', othersKey);
+        computeSharedKey();
+        socket.broadcast.emit('shared-key', sharedKey);
+        break;
+      default:
+        return;
+    }
   });
 });
 
 // Cliente
-const ioc = require("socket.io-client");
+const ioc = require('socket.io-client');
 
 // Se usa para ENVIAR mensajes
 var socketOut = null;
@@ -72,10 +79,10 @@ const encodeDesECB = (textToEncode, keyString) => {
 
   var keyBuffer = Buffer.from(keyString.toString().substring(0, 8));
 
-  var cipher = crypto.createCipheriv("des-ecb", keyBuffer, "");
+  var cipher = crypto.createCipheriv('des-ecb', keyBuffer, '');
 
-  var c = cipher.update(textToEncode, "utf8", "base64");
-  c += cipher.final("base64");
+  var c = cipher.update(textToEncode, 'utf8', 'base64');
+  c += cipher.final('base64');
 
   return c;
 };
@@ -85,14 +92,14 @@ const decodeDesECB = (textToDecode, keyString) => {
 
   const keyBuffer = Buffer.from(keyString.toString().substring(0, 8));
 
-  const cipher = crypto.createDecipheriv("des-ecb", keyBuffer, "");
+  const cipher = crypto.createDecipheriv('des-ecb', keyBuffer, '');
 
-  let c = cipher.update(textToDecode, "base64", "utf8");
+  let c = cipher.update(textToDecode, 'base64', 'utf8');
   try {
-    c += cipher.final("utf8");
+    c += cipher.final('utf8');
   } catch (e) {
     console.error(e);
-    return "No se pudo desencriptar el texto " + textToDecode;
+    return 'No se pudo desencriptar el texto ' + textToDecode;
   }
 
   return c;
@@ -102,12 +109,12 @@ const decodeDesECB = (textToDecode, keyString) => {
 const ALPHA = bigInt(17123207);
 const Q = bigInt(2426697107);
 var isAlice = null;
-var othersKey = "";
-var secretKey = "";
-var sharedKey = "";
+var othersKey = '';
+var secretKey = '';
+var sharedKey = '';
 
 const computePublicKey = (y, a = ALPHA, q = Q) => {
-  console.log("y", y);
+  console.log('y', y);
   if (y >= q) {
     return;
   }
@@ -118,7 +125,7 @@ const computeSharedKey = (a = ALPHA, q = Q) => {
   const bigIntKey = bigInt(othersKey);
   const bigSecretKey = bigInt(secretKey);
   sharedKey = fastExp(bigIntKey, bigSecretKey, q);
-  console.log("la llave compartida es ", sharedKey);
+  console.log('la llave compartida es ', sharedKey);
 };
 
 const fastExp = (base, exp, q) => {
